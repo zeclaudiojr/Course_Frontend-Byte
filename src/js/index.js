abbfe8b754be6d871bede047d94aeb2daa8784bd
João Eudes Lima <joaoeudes7@gmail.com>
2019-10-15 00:36:11 -0300

    merge files js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-import { onShowMsg } from './functions';
+function showAlert() {
+    alert('Já que você é curioso abra o console');
+    console.log('Essas alterações dentro da página estão sendo feitas por meio do JavaScript com o método `setInterval()`');
+}
+
+function onShowMsg() {
+    const msg = document.getElementById('msg');
+    msg.hidden = false;
+
+    showAlert();
+
+    const milliseconds = 2 * 1000;
+    setInterval(() => {
+        msg.innerText += "\n#SouByte"
+    }, milliseconds);
+}
 
 window.onload = () => {
     console.log('Essa página foi feita com Html(Linguagem de marcação), Css, JS! :p');
